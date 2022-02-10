07ed9263553a5af55216dd2966e7993c0b76a728
Nuraiym000 <nurajymmm1@gmail.com>
2022-02-10 17:04:16 +0600

    finaly

---
+++ main/serializers.py
@@ -0,0 +1,104 @@
+from rest_framework import serializers
+
+from main.models import Problema, CodeImage, Reply, Comment
+
+
+class CodeImageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CodeImage
+        fields = ('image',)
+
+    def _get_image_url(self, instance):
+        if instance.image:
+            url = instance.image.url
+            return 'localhost:8000' + url
+
+    def to_representation(self, instance):
+        representation = super(CodeImageSerializer, self).to_representation(instance)
+        representation['image'] = self._get_image_url(instance)
+        return representation
+
+
+
+class ProblemaSerializer(serializers.ModelSerializer):
+    author = serializers.ReadOnlyField(source='author.username')   #ReadOnlyField-значит необязательно к заполнению  #author.username - можно указать номер
+
+    class Meta:
+        model = Problema
+        fields = '__all__'
+        # exclude = ('author',)
+
+    def create(self, validated_data):
+        request = self.context.get('request')
+        images = request.FILES
+        author = request.user
+        problem = Problema.objects.create(author=author,
+                                          **validated_data)
+        for image in images.getlist('images'):
+            CodeImage.objects.create(image=image,
+                                     problema=problem)
+        return problem
+
+    def update(self, instance, validated_data):
+        request = self.context.get('request')
+        images = request.FILES
+        for key,value in validated_data.items():
+            setattr(instance, key, value)
+        if images.getlist('new images'):
+            instance.images.all().delete()
+            for image in images.getlist('new_images'):
+                CodeImage.objects.create(image=image,
+                                         problema=instance)
+        return instance
+
+
+    def to_representation(self, instance):
+        representation = super().to_representation(instance)
+        representation['images'] = CodeImageSerializer(instance.images.all(),
+                                                       many=True).data
+        action = self.context.get('action')
+        if action == 'list':
+            representation['replies'] = instance.replies.count()
+        else:
+            representation['replies'] = ReplySerializer(instance.replies.all(),
+                                                    many=True).data
+        return representation
+
+
+class ReplySerializer(serializers.ModelSerializer):
+    author = serializers.ReadOnlyField(source='author.username')
+
+    class Meta:
+        model = Reply
+        fields = "__all__"
+
+    def create(self, validated_data):
+        author = self.context.get('request').user
+        reply = Reply.objects.create(author=author,
+                                     **validated_data)
+        return reply
+
+    def to_representation(self, instance):
+        representation = super(ReplySerializer, self).to_representation(instance)
+        action = self.context.get('action')
+        if action == 'list':
+            representation['comments'] = instance.commenty.count()
+        else:
+            representation['comments'] = CommentSerializer(instance.commenty.all(),
+                                                           many=True).data
+        return representation
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    author = serializers.ReadOnlyField(source='author.username')
+
+    class Meta:
+        model = Comment
+        fields = "__all__"
+
+    def create(self, validated_data):
+        author = self.context.get('request').user
+        comment = Comment.objects.create(author=author,
+                                     **validated_data)
+        return comment
+
---
+++ main/views.py
@@ -0,0 +1,75 @@
+from django.db.models import Q
+from django.shortcuts import render
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from rest_framework.viewsets import ModelViewSet
+
+from .models import *
+from .serializers import ProblemaSerializer, ReplySerializer, CommentSerializer
+
+
+class ProblemViewSet(ModelViewSet):
+    queryset = Problema.objects.all()
+    serializer_class = ProblemaSerializer
+
+    def get_serializer_context(self):
+        return {'action': self.action}
+
+    @action(detail=False, methods=['get'])
+    def search(self, request):
+        query = request.query_params.get('q')
+        queryset = self.get_queryset().filter(Q(title__icontains=query) |
+                                              Q(description__icontains=query))
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data, status=200)
+
+
+class ReplyViewSet(ModelViewSet):
+    queryset = Reply.objects.all()
+    serializer_class = ReplySerializer
+
+    def get_serializer_context(self):
+        return {'action': self.action}
+
+
+class CommentViewSet(ModelViewSet):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+
+
+
+# from rest_framework.generics import ListAPIView, RetrieveAPIView, CreateAPIView, UpdateAPIView, DestroyAPIView
+#
+# from .models import *
+# from .serializers import ProblemaSerializer
+#
+#
+# class ProblemListView(ListAPIView):
+#     queryset = Problema.objects.all()
+#     serializer_class = ProblemaSerializer
+#
+#
+# class ProblemaDetailView(RetrieveAPIView):
+#     queryset = Problema.objects.all()
+#     serializer_class = ProblemaSerializer
+#
+#
+# class ProblemCreateView(CreateAPIView):
+#     queryset = Problema.objects.all()
+#     serializer_class = ProblemaSerializer
+#
+#     def get_serializer_context(self):
+#         return {'request': self.request}
+#
+#
+# class ProblemUpdateView(UpdateAPIView):
+#     queryset = Problema.objects.all()
+#     serializer_class = ProblemaSerializer
+#
+#     def get_serializer_context(self):
+#         return {'request': self.request}
+#
+#
+# class ProblemDeleteView(DestroyAPIView):
+#     queryset = Problema.objects.all()
+#     serializer_class = ProblemaSerializer
